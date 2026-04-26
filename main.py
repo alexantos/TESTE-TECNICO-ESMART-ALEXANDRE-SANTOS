8a10c5cdb1a434c8d243a4453ae5286a310688b8
alexantos <xandreale09@gmail.com>
2026-04-26 01:09:28 -0300

    Adiciona fluxo otimizado do QThread para chamadas de api. #7

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import sys
-from time import sleep
 
 import requests
 from PySide6.QtWidgets import QPushButton, QWidget, QApplication, QVBoxLayout, QScrollArea, QLabel, QMainWindow
 from PySide6.QtGui import QPixmap
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import Qt, Slot, QObject, QThread, Signal
 
 from service import Service
+
+
+class Requisicao(QObject):
+    finalizado = Signal()
+    atualizar = Signal(object)
+
+    def __init__(self, pagina):
+        super().__init__()
+        self.pagina = pagina
+
+    @Slot()
+    def requisicao_lenta(self):
+        self.carrega_imagens()
+        self.finalizado.emit()
+
+    def carrega_imagens(self):  # TODO: Fazer novos métodos com novas camadas para chamadas
+        service = Service(pagina=self.pagina)
+        request = service.get_requisicao()
+
+        for imagem in request['photos']:
+            response = requests.get(imagem['src']['large'])  # TODO: Melhorar fluxo de chamadas
+
+            imagem = {
+                "label": response.content,
+                "description": imagem['alt']
+            }
+            self.atualizar.emit(imagem)
 
 
 class MainViewer(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.pagina = 1
+
         self.setWindowTitle('Imagens')
         self.resize(800, 600)
 
@@ -24,39 +53,51 @@ class MainViewer(QMainWindow):
 
         self.scroll.setWidget(self.container)
 
-        self.add_button = QPushButton("Carregar imagens")
-        self.add_button.clicked.connect(self.carrega_imagem_click)
-
-        self.carrega_imagens()
+        self.button = QPushButton("Carregar imagens")
+        self.button.clicked.connect(self.start_thread)
 
         main_widget = QWidget()
         main_layout = QVBoxLayout(main_widget)
         main_layout.addWidget(self.scroll)
-        main_layout.addWidget(self.add_button)
+        main_layout.addWidget(self.button)
 
         self.setCentralWidget(main_widget)
 
+    def start_thread(self):
+        self.button.setEnabled(False)
+
+        self.thread = QThread()
+        self.requisicao = Requisicao(pagina=self.pagina)
+
+        self.requisicao.moveToThread(self.thread)
+
+        self.thread.started.connect(self.requisicao.requisicao_lenta)
+        self.requisicao.finalizado.connect(self.thread.quit)
+        self.requisicao.finalizado.connect(self.requisicao.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.requisicao.atualizar.connect(self.update_label)
+        self.thread.finished.connect(self.on_finished)
+
+        self.thread.start()
+
+        self.pagina += 1
+
+    @Slot(object)
+    def update_label(self, imagem):
+        label = QLabel()
+        pixmap = QPixmap()
+        pixmap.loadFromData(imagem['label'])
+        scaled_pixmap = pixmap.scaled(800, 600)
+        label.setPixmap(scaled_pixmap)
+
+        description = QLabel(imagem['description'])
+
+        self.container_layout.addWidget(label)
+        self.container_layout.addWidget(description)
 
     @Slot()
-    def carrega_imagem_click(self):
-        self.carrega_imagens(pagina=2)
-        # sleep(10)
-
-    def carrega_imagens(self, pagina=1): #TODO: Fazer novos métodos com novas camadas para chamadas
-        service = Service(pagina=pagina)
-        request = service.get_requisicao()
-
-        for imagem in request['photos']:
-            label = QLabel()
-            response = requests.get(imagem['src']['large']) #TODO: Melhorar fluxo de chamadas
-            pixmap = QPixmap()
-            pixmap.loadFromData(response.content)
-            scaled_pixmap = pixmap.scaled(800, 600)
-            label.setPixmap(scaled_pixmap)
-            description = QLabel(imagem['alt'])
-
-            self.container_layout.addWidget(label)
-            self.container_layout.addWidget(description)
+    def on_finished(self):
+        self.button.setEnabled(True)
 
 
 if __name__ == "__main__":
